93aca3c7d48c952df897f9bbd51fbb9c1ea6c67e
Lukanko Dagmara <dagmaraluk@purpleocean.net>
2022-12-12 12:42:05 +0900

    feat: more exercises

--- ExpressEx2/server.js
+++ ExpressEx2/server.js
@@ -2,6 +2,15 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 
+app.use((req, res, next) => {
+  const start = Date.now();
+  next();
+  const delta = Date.now() - start;
+  console.log(`${req.method}, ${req.url}, ${delta}`);
+});
+app.use(express.json());
+const msgs = [];
+
 app.post("/", (req, res) => {
   res.send("Hi");
 });
@@ -9,7 +18,15 @@ app.post("/", (req, res) => {
 app.get("/msg", (req, res) => res.send("<ul><li>A</li></ul>"));
 
 app.post("/msg", (req, res) => {
-  console.log("restarting");
+  if (!req.body.msgText) {
+    res.status(400).json({
+      error: "Missing Msg Text",
+    });
+  }
+  const newMsg = {
+    msgId: msgs.length,
+  };
+  res.json(newMsg);
 });
 
 app.get("/msg/:msgId", (req, res) => {
